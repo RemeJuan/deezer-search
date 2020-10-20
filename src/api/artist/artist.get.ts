efa61e1b7ff02e053c326ced42814f7a30394589
Reme Le Hane <reme.lehane@wyzetalk.com>
2020-10-20 11:56:30 +0200

    feat: added artist tracks list endpoint and interface

--- src/api/artist/artist.get.ts
+++ src/api/artist/artist.get.ts
@@ -1,4 +1,4 @@
-import {IArtist} from './artist.interface';
+import {IArtist, IArtistTracks} from './artist.interface';
 import {BASE_URL, DEFAULT_HEADERS} from '../api.constants';
 
 export const artistSearch = async (query: string): Promise<IArtist[]> => {
@@ -7,6 +7,18 @@ export const artistSearch = async (query: string): Promise<IArtist[]> => {
     headers: DEFAULT_HEADERS,
   }
   const url = `${BASE_URL}/search/artist?limit=9&q=${query}`;
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json.data;
+}
+
+
+export const artistTracks = async (artistId: number): Promise<IArtistTracks[]> => {
+  const options = {
+    method: 'GET',
+    headers: DEFAULT_HEADERS,
+  }
+  const url = `${BASE_URL}/artist/${artistId}/top?limit=5`;
   const response = await fetch(url, options);
   const json = await response.json();
   return json.data;
--- src/api/artist/artist.interface.ts
+++ src/api/artist/artist.interface.ts
@@ -13,3 +13,59 @@ export interface IArtist {
   tracklist: string;
   type: string;
 }
+
+export interface IArtistTracks {
+  "id": number,
+  "readable": boolean,
+  "title": string,
+  "title_short": string,
+  "title_version": string,
+  "link": string,
+  "duration": number,
+  "rank": number,
+  "explicit_lyrics": boolean,
+  "explicit_content_lyrics": number,
+  "explicit_content_cover": number,
+  "preview": string,
+  contributors: IArtistTracksContributor[],
+  "md5_image": string,
+  artist: IArtistTracksArtist,
+  album: IArtistTracksAlbum,
+  type: string;
+}
+
+export interface IArtistTracksContributor {
+  "id": number,
+  "name": string,
+  "link": string,
+  "share": string,
+  "picture": string,
+  "picture_small": string,
+  "picture_medium": string,
+  "picture_big": string,
+  "picture_xl": string,
+  "radio": true,
+  "tracklist": string,
+  "type": string,
+  "role": string
+}
+
+export interface IArtistTracksArtist {
+  "id": number,
+  "name": string,
+  "tracklist": string,
+  "type": string
+}
+
+export interface IArtistTracksAlbum {
+  "id": number,
+  "title": string,
+  "cover": string,
+  "cover_small": string,
+  "cover_medium": string,
+  "cover_big": string,
+  "cover_xl": string,
+  "md5_image": string,
+  "tracklist":string,
+  "type": string
+}
