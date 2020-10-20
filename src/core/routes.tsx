269ea4161aeca19cf3232f87b0786838438af86c
Reme Le Hane <reme.lehane@wyzetalk.com>
2020-10-20 11:08:41 +0200

    feat: added app routes

---
+++ src/core/routes.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import SearchPage from '../pages/search.page';
+import ArtistPage from '../pages/artist.page';
+
+export const routes = {
+  main: '/search/:query?',
+  artist: 'artis/:name'
+}
+
+export default () => (
+  <Switch>
+    <Route path={routes.main}>
+      <SearchPage/>
+    </Route>
+    <Route path={routes.artist}>
+      <ArtistPage/>
+    </Route>
+    <Route path="*">
+      <Redirect to='/search' />
+    </Route>
+  </Switch>
+)
