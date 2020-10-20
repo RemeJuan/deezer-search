7e98f4d75ceb2d91ba4eab3f2f59e24c1d5a81f5
Reme Le Hane <reme.lehane@wyzetalk.com>
2020-10-20 12:02:13 +0200

    fix: added loading indicator

--- src/pages/search/search.page.tsx
+++ src/pages/search/search.page.tsx
@@ -6,28 +6,35 @@ import Card from '../../components/artist-card/artist-card';
 import {Container} from '@material-ui/core';
 import {searchStyles} from './search.styles';
 import Grid from '@material-ui/core/Grid';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 export default () => {
   const classes = searchStyles();
   const [artists, setArtists] = useState<IArtist[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   
   const handleSearch = async (query: string) => {
+    setLoading(true);
     const results = await artistSearch(query);
 
     setArtists(results);
+    setLoading(false);
   }
   
   return (
     <>
       <AppBar handleSubmit={handleSearch}/>
   
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
-          {artists.map(artist => (
-            <Card key={artist.id} artist={artist}/>
-          ))}
-        </Grid>
-      </Container>
+      {loading ?
+        <LinearProgress/> :
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {artists.map(artist => (
+              <Card key={artist.id} artist={artist}/>
+            ))}
+          </Grid>
+        </Container>
+      }
     </>
   );
 }
